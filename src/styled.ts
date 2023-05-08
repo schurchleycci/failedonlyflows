34e935f23decb32d17bfdfadf2384bc809fe109d
Scott Churchley <scott.churchley@circleci.com>
2023-05-08 10:12:26 -0400

    Updates

--- src/styled.ts
+++
@@ -1,5 +0,0 @@
-import { CompassTheme } from '@circleci/compass';
-import Theme from '@circleci/theme';
-import styled, { CreateStyled } from '@emotion/styled';
-
-export default styled as CreateStyled<typeof Theme & CompassTheme>;
